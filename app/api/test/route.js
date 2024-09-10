228064d894f80d0d62a280d18aeff235cd56b582
Muhammad Rafai <rafaimhd123@gmail.com>
2024-09-10 23:13:59 +0700

    add responsive design for hero banner

---
+++ app/api/test/route.js
@@ -0,0 +1,8 @@
+export async function GET (req) {
+
+    return Response.json({ name: 'John Doe', error: false })
+}
+export async function POST (req) {
+    
+    return Response.json({ name: 'John Doe', error: false })
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import HeroBanner from "@/components/HeroBanner";
+
+export default function Home() {
+  return (
+    <>
+      <div className="fixed">Navbar </div>
+      <HeroBanner />
+    </>
+  );
+}
